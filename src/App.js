a743ab82e9728396ff3bc751403e8487b6f745b8
Jean Yves Gatwaza <jgatwazakubwimana@college.harvard.edu>
2024-04-24 22:00:35 -0400

    Last deployment didnt't go as expected

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import './App.css';
 import React from 'react';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Challenge from './components/Challenge';
 import Score from './components/Score';
 import Leaderboard from './components/Leaderboard';
 import Register from './components/Register';
 import ProtectedRoute from './components/ProtectedRoute';
-
 import { AuthProvider } from './AuthContext';
 import EnterNamePage from './components/EnterNamePage';
+
+const basename = "/jGatwaza/AuthentiQuest";
+
+
 function App() {
   return (
     <AuthProvider>
-    <Router>
-      <div>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/challenge" element={<ProtectedRoute component={() => <Challenge />} />} />
+          <Route path="/challenge" element={<ProtectedRoute component={Challenge} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/score" element={<Score />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/enternamepage" element={<EnterNamePage />} />
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
