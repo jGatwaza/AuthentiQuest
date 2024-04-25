44d8fd8250d81462bcb10d51a140f56447d60c6e
Jean Yves Gatwaza <jgatwazakubwimana@college.harvard.edu>
2024-04-24 21:32:35 -0400

    Updates

---
+++ precache-manifest.3caa2a3c2adf19a06955e81f3ac2fc7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9848c5ab450fe76f6f0f95b7000988",
+    "url": "/jGatwaza/AuthentiQuest/index.html"
+  },
+  {
+    "revision": "3f2bc4eb85783e55ba29",
+    "url": "/jGatwaza/AuthentiQuest/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "6d2a96cf35a3f502b654",
+    "url": "/jGatwaza/AuthentiQuest/static/css/main.e2a96f3d.chunk.css"
+  },
+  {
+    "revision": "3f2bc4eb85783e55ba29",
+    "url": "/jGatwaza/AuthentiQuest/static/js/2.ff9ac0fa.chunk.js"
+  },
+  {
+    "revision": "1139c9d123f59fd21bd01beec0a0d448",
+    "url": "/jGatwaza/AuthentiQuest/static/js/2.ff9ac0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b5d15a894a51d0cda2",
+    "url": "/jGatwaza/AuthentiQuest/static/js/3.e62aba85.chunk.js"
+  },
+  {
+    "revision": "6d2a96cf35a3f502b654",
+    "url": "/jGatwaza/AuthentiQuest/static/js/main.12aee5a4.chunk.js"
+  },
+  {
+    "revision": "a9476b2e37e870946377",
+    "url": "/jGatwaza/AuthentiQuest/static/js/runtime-main.994d6773.js"
+  }
+]);
